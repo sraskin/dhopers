2ce57aacc5d89f0fdfaadf17bad206d47cc32760
Irfan Ahmed <irfandhk@gmail.com>
2015-12-04 16:33:47 +0600

    added sms notification

--- app/models/order.rb
+++ app/models/order.rb
@@ -14,14 +14,14 @@ class Order < ActiveRecord::Base
 
   validates :phone, :presence => true
 
-  ACCEPTED = :accepted
-  REQUESTED = :requested
+  ACCEPTED   = :accepted
+  REQUESTED  = :requested
   COLLECTING = :collecting
   PROCESSING = :processing
-  PROCESSED = :processed
-  DELIVERED = :delivered
-  COMPLETED = :completed
-  CANCELLED = :cancelled
+  PROCESSED  = :processed
+  DELIVERED  = :delivered
+  COMPLETED  = :completed
+  CANCELLED  = :cancelled
 
   ORDER_BASE_NUMBER = 10000
 
@@ -52,7 +52,7 @@ class Order < ActiveRecord::Base
     end
 
     event :mark_as_delivered do
-      transitions :from =>  Order::PROCESSED, :to => Order::DELIVERED
+      transitions :from => Order::PROCESSED, :to => Order::DELIVERED
     end
 
     event :close do
@@ -67,7 +67,7 @@ class Order < ActiveRecord::Base
 
   def assign_user_information
     self.full_name = self.user.full_name
-    self.email = self.user.email
+    self.email     = self.user.email
   end
 
   def to_param
@@ -78,21 +78,48 @@ class Order < ActiveRecord::Base
     Rails.application.routes.url_helpers.customer_order_url(self)
   end
 
-  private
+  def order_sms_txt
+    if self.aasm_state == Order::ACCEPTED.to_s
+      return "Your requested order(#{self.order_number}) has been accepted. - Dhopers.com"
+    elsif self.aasm_state == Order::PROCESSED.to_s
+      return "Your order(#{self.order_number}) is ready delivery - Dhopers.com"
+    elsif self.aasm_state == Order::DELIVERED.to_s
+      return "Your order(#{self.order_number}) has been delivered. Thank you being with us- Dhopers.com"
+    else
+      return false
+    end
+  end
 
   def send_order_notification
+    if self.order_sms_txt
+      self.sms_service.send_sms(self.phone, self.order_sms_txt)
+    end
+
     if self.aasm_state == Order::ACCEPTED.to_s
-      OrderEventMailer.order_received(self).deliver
+      OrderEventMailer.order_received(self).deliver_now
     elsif self.aasm_state == Order::PROCESSED.to_s
-      OrderEventMailer.order_processed(self).deliver
+      OrderEventMailer.order_processed(self).deliver_now
     elsif self.aasm_state == Order::DELIVERED.to_s
-      OrderEventMailer.order_delivery_notification(self).deliver
+      OrderEventMailer.order_delivery_notification(self).deliver_now
       create_payment_history
     end
   end
 
+  def send_sms
+    puts self.order_sms_txt
+    self.sms_service.send_sms(self.phone, self.order_sms_txt)
+  end
+
+  protected
+
+  def sms_service
+    @sms = SmsService.new
+  end
+
+  private
+
   def trigger_order_events
-    self.order_events.create!(current_state: self.aasm_state, previous_state: self.aasm_state_was || 'requested' ,user: self.user)
+    self.order_events.create!(current_state: self.aasm_state, previous_state: self.aasm_state_was || 'requested', user: self.user)
   end
 
   def generate_order_num
@@ -108,7 +135,7 @@ class Order < ActiveRecord::Base
         {
             due:         true,
             amount:      self.amount,
-            notes:        "Payment for Order##{self.order_number}",
+            notes:       "Payment for Order##{self.order_number}",
             discount:    0,
             final_price: self.amount
         }
--- app/models/sms_service.rb
+++ app/models/sms_service.rb
@@ -12,18 +12,20 @@ class SmsService
   attr_reader :msg_type
   attr_reader :field_cnt
 
-  ERRORS             = {}
-  ERRORS['ERROR100'] = 'Temporary Internal Server Error. Try again later'.freeze
-  ERRORS['ERROR101'] = 'Authentication Error (Not valid login Information)'.freeze
-  ERRORS['ERROR102'] = 'No credits available'.freeze
-  ERRORS['ERROR103'] = 'MSIDSN (phone parameter) is invalid or prefix is not supported'.freeze
-  ERRORS['ERROR104'] = 'Tariff Error'.freeze
-  ERRORS['ERROR105'] = 'You are not allowed to send to that destination/country'.freeze
-  ERRORS['ERROR106'] = 'Not Valid Route number or you are not allowed to use this route'.freeze
-  ERRORS['ERROR107'] = 'No proper Authentication (IP restriction is activated)'.freeze
-  ERRORS['ERROR108'] = 'You have no permission to send messages through HTTP API'.freeze
-  ERRORS['ERROR109'] = 'Not Valid Originator'.freeze
-  ERRORS['ERROR999'] = 'Invalid HTTP Request'.freeze
+  RESPONSE             = {}
+  RESPONSE['ERROR100'] = 'Temporary Internal Server Error. Try again later'.freeze
+  RESPONSE['ERROR101'] = 'Authentication Error (Not valid login Information)'.freeze
+  RESPONSE['ERROR102'] = 'No credits available'.freeze
+  RESPONSE['ERROR103'] = 'MSIDSN (phone parameter) is invalid or prefix is not supported'.freeze
+  RESPONSE['ERROR104'] = 'Tariff Error'.freeze
+  RESPONSE['ERROR105'] = 'You are not allowed to send to that destination/country'.freeze
+  RESPONSE['ERROR106'] = 'Not Valid Route number or you are not allowed to use this route'.freeze
+  RESPONSE['ERROR107'] = 'No proper Authentication (IP restriction is activated)'.freeze
+  RESPONSE['ERROR108'] = 'You have no permission to send messages through HTTP API'.freeze
+  RESPONSE['ERROR109'] = 'Not Valid Originator'.freeze
+  RESPONSE['ERROR999'] = 'Invalid HTTP Request'.freeze
+  RESPONSE['INVALID'] = 'Unknown error'.freeze
+  RESPONSE['OK']       = 'Sms has been sent successfully'
 
   def initialize(username = '', password = '', provider = '')
     @username    = username
@@ -34,10 +36,39 @@ class SmsService
     @charset     = 0
     @utc         = 0
     @msg_type    = ''
-    @field_cnt = 8
+    @field_cnt   = 8
   end
 
   def send_sms(recipient, msg_text, showDLR = 1)
-    query_str =  "/?username=#{self.username}&password=#{self.password}&charset=#{self.charset}&msgtext=#{msg_text}&originator=#{ORIGINATOR}&phone=#{recipient}&provider=#{self.provider}&showDLR=#{showDLR}&msgtype=#{self.msg_type}"
+    res = nil
+    query_str = "http://manage.muthofun.com/bulksms/bulksend.go?username=odesk.irfan@gmail.com&password=bonomali1247&originator=dhopers.com&phone=#{recipient}&msgtext=#{msg_text}"
+
+    begin
+      conn      = Faraday.new(:url => query_str)
+      res = conn.get
+    rescue => e
+      return RESPONSE['INVALID']
+    end
+
+    return RESPONSE[res.body]
+  end
+
+  def test_sms_url
+    'http://manage.muthofun.com/test.html'
+  end
+
+  def get_balance
+    query_str = "http://manage.muthofun.com/bulksms/getBALANCE.go?username=odesk.irfan@gmail.com&password=bonomali1247&originator=dhopers.com"
+
+    res = nil
+
+    begin
+      conn      = Faraday.new(:url => query_str)
+      res = conn.get
+    rescue => e
+      return RESPONSE['INVALID']
+    end
+
+    return res.body
   end
 end
